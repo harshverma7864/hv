21f7bef04ae2c3fe3580541aba5154a6f3399f11
harshverma7864 <83391423+harshverma7864@users.noreply.github.com>
2022-07-31 21:44:33 +0530

    Update [slug].js

--- homelyvirtuals/pages/categories/[slug].js
+++ homelyvirtuals/pages/categories/[slug].js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import Navbar from '../components/navbar';
+import Navbar from '../components/Navbar';
 
 const categories = ({category}) => {
   const router = useRouter();
